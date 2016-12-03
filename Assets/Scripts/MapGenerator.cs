578608b91fae4a71d3ede4f40e1ee354f2418731
olivierLefebvre96 <olivier.lefebvre@akeonet.com>
2016-12-03 13:44:24 +0100

    Version avec bruit de Palin

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public int octaves;
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+
+    void OnValidate() {
+        if (mapWidth < 1) {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1) {
+            mapHeight = 1;
+        }
+    }
+}
